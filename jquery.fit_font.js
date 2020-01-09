4ee0f723ad71a0b26634e8262017828fccc61b20
Oria <oriadam@gmail.com>
2020-01-09 12:35:51 +0200

    Update jquery.fit_font.js
    edge case when changing the font size did not change the width, but trying again might do the trick. i could only reproduce it when zooming browser.

--- jquery.fit_font.js
+++ jquery.fit_font.js
@@ -6,10 +6,10 @@
 	{
 		options = $.extend({
 			multiline: true, // ignore height when false
-			max: 2, // max font size value
-			min: 1, // min font size value
-			unit: 'em', // units for the font size
-			step: /v|px|%/.test(options.unit) ? 1 : .1, // steps to take when trying sizes
+			max: 24, // max font size value
+			min: 15, // min font size value
+			unit: 'px', // units for the font size
+			step: /em/.test(options.unit) ? .1 : 1, // steps to take when trying sizes
 			width: null, // desired width. defaults to element width
 			height: null, // on multiline mode, desired element height. defaults to element width
 			hide_while_working: true // set visibility false while looking for the correct size so the user won't see the font decreases
@@ -29,15 +29,20 @@
 			if (!options.multiline)
 				elem.style.whiteSpace = 'nowrap';
 			elem.style.fontSize = size + options.unit;
-			while (max_loops-- && size > options.min && ((elem.scrollWidth > width) || (options.multiline ? elem.scrollHeight > height : false)))
+			while (max_loops-- && size > options.min && ((elem.scrollWidth > 1+width) || (options.multiline ? elem.scrollHeight > 1+height : false)))
 			{
 				size -= options.step;
 				previous = [elem.scrollWidth, elem.scrollHeight];
 				elem.style.fontSize = size + options.unit;
 				if (elem.scrollWidth === previous[0] && elem.scrollHeight === previous[1])
 				{
-					console.log('fit_font error 101: element has important font size or step parameter is too small', elem); // if w/h haven't changed, quit - our efforts are in vain
-					break;
+					size -= options.step; // try again to make a change
+					elem.style.fontSize = size + options.unit;
+					if (elem.scrollWidth === previous[0] && elem.scrollHeight === previous[1])
+					{
+						console.log('fit_font error 101: element has important font size or step parameter is too small', elem); // if w/h haven't changed, quit - our efforts are in vain
+						break;
+					}
 				}
 				if (options.multiline && !options.height && height !== $(elem).outerHeight())
 				{
